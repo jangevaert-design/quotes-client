670375e3d1c259b790b1f1c33ed590a4c7f5e4a0
Jan Gevaert <jan.gevaert@gmail.com>
2020-07-14 10:26:04 -0600

    Oauth2 working front and back

--- app/src/androidTest/java/edu/cnm/deepdive/quotes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/cnm/deepdive/quotes/ExampleInstrumentedTest.java
@@ -2,14 +2,15 @@ package edu.cnm.deepdive.quotes;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+
+import androidx.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
+/**e
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
